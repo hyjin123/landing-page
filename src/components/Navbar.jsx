2e6a38cfdb8b3df8e4d49fcc35680633888e4f0d
Sean <seanhoyeonjin@gmail.com>
2024-01-05 20:35:05 -0500

    navbar is completed for now

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,55 @@ import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  console.log(toggle);
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+      {/* this is hidden for mobile (small screens) */}
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${
+              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+            }`}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+
+      {/* sm:hidden will hide this element ONLY when screen exceeds 640px */}
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px]"
+          onClick={() => setToggle((prev) => !prev)}
+        />
+
+        {/* This is the toggle menu */}
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex flex-col justify-end items-center flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${
+                  index === navLinks.length - 1 ? "mb-0" : "mb-4"
+                }`}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
